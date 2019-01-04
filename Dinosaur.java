3baf7484e582ff64664342bb7bd0578bf6e6a8d7
Amara Shein <ashein00@stuy.edu>
2019-01-04 10:08:55 -0500

    finished dinosaur constructor and get methods

--- Dinosaur.java
+++ Dinosaur.java
@@ -8,7 +8,34 @@ public class Dinosaur{
   
   /**
   * Represents how fast a dinosaur is.
-  * Gradually, the speed will increase with time to increase the difficulty of the game
+  * Gradually, the speed will increase with time to increase the difficulty of the game.
   */ 
   private double speed;
+  
+  /**
+  * Creates a dinosaur and intializes its health and speed.
+  * @param health_ is a dinosaur's health.
+  * @param speed_ is a dinosaur's speed.
+  */
+  public Dinosaur(int health_, double speed_){
+    health = health_;
+    speed = speed_;
+  }
+
+  /**
+  * Gets the health of a dinosaur at any given time.
+  * @return the health of a dinosaur.
+  */
+  public int getHealth(){
+    return health;
+  }
+
+  /**
+  * Gets the speed of a dinosaur at any given time.
+  * @return the speed of a dinosaur.
+  */
+  public double getSpeed(){
+    return speed;
+  }
 }
+
