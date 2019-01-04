b576c7e1603f8346120b0de509210c76254c682f
Amara Shein <ashein00@stuy.edu>
2019-01-04 09:53:27 -0500

    javadoc for cactus.java

--- Cactus.java
+++ Cactus.java
@@ -1,5 +1,26 @@
 public class Cactus{
-  private double height;
-  private double width;
+  /**
+  * Represents how tall a cactus is.
+  * One unit is equivalent to one new line.
+  */
+  private int height;
+
+  /**
+  * Represents how wide a cactus is.
+  * One unit is equivalent to one space.
+  */
+  private int width;
+  
+  /**
+  * Represents the type of cactus or set of cacti.
+  * Types will determine the height, width, and number of consecutive cacti.
+  */
   private int type;
+
+  /**
+  * Initializes a cactus as the specified type. 
+  * @param givenType is the numerical representation that corresponds to a set height, width, and number of consecutive cacti.
+  */
+  public Cactus(int givenType){
+  }
 }
