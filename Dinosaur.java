982456f76d178e0cdb92e37fb4924bed8a22eb40
Amara Shein <ashein00@stuy.edu>
2019-01-04 09:46:50 -0500

    last push didn't work; adding javadoc

--- Dinosaur.java
+++ Dinosaur.java
@@ -1,5 +1,19 @@
 public class Dinosaur{
+  /**
+  * Represents how much health a dinosaur has. 
+  * Colliding with an obstacle will make the health zero.
+  * Zero health will cause the game to terminate.
+  */
   private int health;
+  
+  /**
+  * Represents how fast a dinosaur is.
+  * Gradually, the speed will increase with time to increase the difficulty of the game
+  */ 
   private double speed;
+
+// unnecessary for now?
+  /*
   private int power;
+  */
 }
