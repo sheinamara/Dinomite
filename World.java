3210ef7d7dd39ace4ea6670c12a382b3472557b5
Jeff Lin <jlin02@stuy.edu>
2019-01-06 23:25:29 -0500

    Added lanterna 3.0 jar file and created World class with floor.

---
+++ World.java
@@ -0,0 +1,58 @@
+import com.googlecode.lanterna.*;
+import com.googlecode.lanterna.input.*;
+import com.googlecode.lanterna.terminal.*;
+import com.googlecode.lanterna.screen.*;
+import com.googlecode.lanterna.graphics.*;
+import java.io.IOException;
+import java.awt.Color;
+
+
+public class World {
+
+	public static void putString(int x, int y, Screen screen, String str) {
+		for (int i = 0; i < str.length(); ++i) {
+			screen.setCharacter(x+i, y, new TextCharacter(str.charAt(i)));
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		Screen screen = new DefaultTerminalFactory().createScreen();
+		screen.startScreen();
+
+		long tStart = System.currentTimeMillis();
+		long lastSecond = 0;
+
+
+		TerminalSize size = screen.getTerminalSize();
+		TextGraphics tg = screen.newTextGraphics();
+		tg.drawLine(0,size.getRows()-1,size.getColumns()-1,size.getRows()-1,'\u2571');
+		tg.drawLine(0,size.getRows()-2,size.getColumns()-1,size.getRows()-2,'\u2571');
+		tg.drawLine(0,size.getRows()-3,size.getColumns()-1,size.getRows()-3,'\u2581');
+
+		while (true) {
+
+			// User Input
+			KeyStroke key = screen.pollInput();
+			if (key != null) {
+				screen.setCharacter(x, y, new TextCharacter(' '));
+
+				if      (key.getKeyType() == KeyType.Escape) break;
+				else if (key.getKeyType() == KeyType.ArrowUp) y--;
+
+				putString(1, 1, screen, key+"                 ");
+
+			}
+
+			// Score Keeping
+			long tEnd = System.currentTimeMillis();
+			long millis = tEnd - tStart;
+			putString(1, 2, screen, "Score: "+ millis / 100);
+			screen.doResizeIfNecessary();
+			screen.refresh();
+		}
+
+		screen.stopScreen();
+	}
+
+}
